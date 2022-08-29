14714f72dd8257472e25ac17f78c23eae441d240
Murder O Crows <raven@Murders-MacBook-Pro.local>
2022-08-29 03:14:02 -0700

    initial server done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT
+app.get('/', (req, res) => {
+  res.send('express TS Server...')
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https:localhost:${port}`)
+})
